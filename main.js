7b355f7e5107192d139b4c4bf7c89822b6fdc0e9
sk0ut <andreas.hyys@gmail.com>
2022-04-01 19:45:25 +0300

    cleanup before readme

--- main.js
+++ main.js
@@ -29,7 +29,7 @@ app.get('/parts', (req, res) => {
     }
 
     var params = req.query
-    //default
+
     var pageMarker = 0
     var pageLimit = 10
     var sort = "name"
@@ -97,11 +97,16 @@ app.get('/parts', (req, res) => {
     })
 });
 
+app.get('*', (req, res) => {
+    return res.json({
+        "code": 404,
+        "message": "Invalid endpoint"
+    })
+})
+
 app.listen("3030", () => {
     console.log(`Example app listening on port 3030}`)
 })
-
-// functions 
 
 function dynamicSort(property) {
     var sortOrder = 1;
@@ -110,25 +115,15 @@ function dynamicSort(property) {
         property = property.substr(1);
     }
     return function (a,b) {
-        /* next line works with strings and numbers, 
-         * and you may want to customize it to your needs
-         */
         var result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
         return result * sortOrder;
     }
 }
 
 function dynamicSortMultiple(props) {
-    /*
-     * save the arguments object as it will be overwritten
-     * note that arguments object is an array-like object
-     * consisting of the names of the properties to sort by
-     */
     return function (obj1, obj2) {
         var i = 0, result = 0, numberOfProperties = props.length;
-        /* try getting a different result from 0 (equal)
-         * as long as we have extra properties to compare
-         */
+
         while(result === 0 && i < numberOfProperties) {
             result = dynamicSort(props[i])(obj1, obj2);
             i++;
